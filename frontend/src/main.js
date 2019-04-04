8380098f5297ccc17e81739fa6337456fb80e52c
Saksham <gupta.10@iitj.ac.in>
2019-04-05 00:12:47 +0530

    Added basic Calendar
    Calendar functionality added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,24 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import './plugins/vuetify'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'dayspan-vuetify/dist/lib/dayspan-vuetify.min.css'
+import DaySpanVuetify from 'dayspan-vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(DaySpanVuetify, {
+  methods: {
+    getDefaultEventColor: () => '#1976d2'
+  }
+});
+
+Vue.use(Vuetify);
 
 new Vue({
   router,
