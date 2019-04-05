f5e36c9e30600d175c326810da84bb2009cc9564
kavishgambhir <kavishgambhir21@gmail.com>
2019-04-06 02:15:34 +0530

    modified StudentProfile model

--- backend/account/models.py
+++ backend/account/models.py
@@ -6,7 +6,7 @@ class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     roll_number = models.CharField(max_length=10, unique=True)
     dob = models.DateField()
-    avatar = models.ImageField(upload_to='avatar')
+    avatar = models.ImageField(upload_to='avatar',null=True)
     department = models.ForeignKey('department.Department', on_delete=models.CASCADE)
 
     def __str__(self):
