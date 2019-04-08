98119d030addff47dbfc302caeb26c06ccd30be5
Saksham <gupta.10@iitj.ac.in>
2019-04-09 00:50:06 +0530

    Local changes

--- frontend/src/views/Calendar.vue
+++ frontend/src/views/Calendar.vue
@@ -1,12 +1,15 @@
 <template>
     <!-- <p>asdfs</p> -->
-    <ds-calendar-app :calendar="calendar"></ds-calendar-app>
+    <ds-calendar-app :calendar="calendar" fixed app></ds-calendar-app>
 </template>
 <style>
 body, html, #dayspan {
   font-family: Roboto, sans-serif;
   width: 100%;
   height: 100%;
+}
+.ds-calendar-app > .ds-app-calendar-toolbar{
+    visibility: hidden!important;
 }
 </style>
 <script>
