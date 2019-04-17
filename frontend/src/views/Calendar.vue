3ff07bf22413e33668cb1a80addd809931bcaabc
Saksham <gupta.10@iitj.ac.in>
2019-04-17 22:12:16 +0530

    Calendar Changes

--- frontend/src/views/Calendar.vue
+++ frontend/src/views/Calendar.vue
@@ -1,231 +1,155 @@
-
 <template>
-<body class="text-center body">
-  <div id="calendar">
-    <div class="header">
-      <button v-on:click="prevMonth()">Hi</button>
-      {{ moment(currentMonth+1,'MM').format('MMMM') }} {{currentYear}}
-      <button
-        v-on:click="nextMonth()"
-      >></button>
-    </div>
-    <div v-for="(week, k) in calendar" class="week" :key="k">
-      <div
-        v-for="(day, i) in week.days"
-        v-on:click="selectDay(day)"
-        class="day"
-        :key="i"
-        v-bind:class="[{in : isSameMonth(day.date)}, 
-                             {active : day.date == activeDay}]"
-      >
-        <div class="day-name">{{moment(day.date).format('ddd')}}</div>
-        <div class="day-number">{{moment(day.date).format('DD')}}</div>
-        <div class="day-event">
-          <div class="active-event" v-if="day.event.length"></div>
-        </div>
-      </div>
-    </div>
-
-    <div class="seleted-day">{{daySelected}}</div>
-  </div>
-</body>
+  <v-layout>
+    <v-flex>
+      <v-sheet height="500">
+        <v-calendar
+          :now="today"
+          :value="today"
+          color="primary"
+        >
+          <template v-slot:day="{ date }">
+            <template v-for="event in eventsMap[date]">
+              <v-menu
+                :key="event.title"
+                v-model="event.open"
+                full-width
+                offset-x
+              >
+                <template v-slot:activator="{ on }">
+                  <div
+                    v-if="!event.time"
+                    v-ripple
+                    class="my-event"
+                    v-on="on"
+                    v-html="event.title"
+                  ></div>
+                </template>
+                <v-card
+                  color="grey lighten-4"
+                  min-width="350px"
+                  flat
+                >
+                  <v-toolbar
+                    color="primary"
+                    dark
+                  >
+                    <v-btn color="red" icon>
+                      <v-icon>edit</v-icon>
+                    </v-btn>
+                    <v-toolbar-title v-html="event.title"></v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-btn icon>
+                      <v-icon>favorite</v-icon>
+                    </v-btn>
+                    <v-btn icon>
+                      <v-icon>more_vert</v-icon>
+                    </v-btn>
+                  </v-toolbar>
+                  <v-card-title primary-title>
+                    <span v-html="event.details"></span>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-btn
+                      flat
+                      color="secondary"
+                    >
+                      Cancel
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-menu>
+            </template>
+          </template>
+        </v-calendar>
+      </v-sheet>
+    </v-flex>
+  </v-layout>
 </template>
 
-<style>
-#calendar {
-  width: 420px;
-  margin-left: 30px;
-  height: 570px;
-  margin-top: 50px;
-
-  .header {
-    display: flex;
-    justify-content: space-between;
-    height: 30px;
-    text-transform: uppercase;
+<style lang="stylus" scoped>
+  .my-event {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    border-radius: 2px;
+    background-color: #1867c0;
+    color: #ffffff;
+    border: 1px solid #1867c0;
+    width: 100%;
+    font-size: 12px;
+    padding: 3px;
+    cursor: pointer;
+    margin-bottom: 1px;
   }
-
-  .week {
-    display: grid;
-    grid-template-columns: auto auto auto auto auto auto auto;
-    justify-items: center;
-    align-items: center;
-    .day {
-      padding: 10px;
-      padding-bottom: 8px;
-      width: 40px;
-      border-radius: 14px;
-      transition: all 0.3s ease-in-out;
-
-      &:hover {
-        cursor: pointer;
-      }
-      &.active {
-        box-shadow: 0px 0px 15px rgba(210, 210, 210, 0.5);
-        .day-number {
-          color: #31b3ab;
-        }
-      }
-    }
-  }
-}
-
-.day {
-  text-align: center;
-  &:not(.in) {
-    color: rgba(52, 52, 52, 0.5);
-  }
-
-  .day-name {
-    font-size: 0.5rem;
-    text-transform: uppercase;
-    margin-bottom: 5px;
-    color: rgba(52, 52, 52, 0.5);
-    letter-spacing: 0.7px;
-  }
-  .day-number {
-    font-size: 1.5rem;
-    letter-spacing: 1.5px;
-  }
-  .day-event {
-    min-heigth: 0.5rem;
-    position: relative;
-    margin-bottom: 0.5rem;
-
-    .active-event {
-      position: absolute;
-      top: 0;
-      left: 50%;
-      transform: translateX(-50%);
-      width: 0.5rem;
-      height: 0.5rem;
-      background-color: #ffc200;
-    }
-  }
-}
 </style>
 
 <script>
-import moment from 'moment'
-
-export default {
-  name: "Calendar",
-  data() {
-    return {
-      startWeek: moment,
-      activeDay: "",
-      endWeek: moment,
-      calendar: [],
-      today: moment(),
-      currentMonth: "",
-      currentYear: "",
-      daySelected: "",
-      moment: moment,
-      events: [{
-                date: '02-10-2019',
-                event: 'coucou les ptits loups'
-              },{
-                date: '03-16-2019',
-                event: 'Boom Party'
-              },
-               ]
-    };
-  },
-  methods: {
-    setMounthWeek: function() {
-      this.startWeek = this.today
-        .clone()
-        .startOf("month")
-        .clone()
-        .week();
-      this.endWeek = this.today
-        .clone()
-        .endOf("month")
-        .clone()
-        .week();
-
-      if (this.today.month() === 11) {
-        this.endWeek = 53;
-      }
-      this.calendar = [];
-      for (var week = this.startWeek; week < this.endWeek + 1; week++) {
-        console.log(week);
-        this.calendar.push({
-          week: week,
-          days: this.getDay(week)
-        });
-      }
-    },
-    getDay: function(week) {
-      return (Array(7)
-        .fill(0)
-        .map((n, i) => {
-          let day = this.today
-            .clone()
-            .week(week)
-            .startOf("week")
-            .clone()
-            .add(n + i, "day")
-            .clone();
-          let event = this.getDayEvent(day);
-          return { date: day, event: event };
-        }));
-    },
-    getDayEvent: function(day) {
-      return this.events.filter(ev => {
-        if (day.isSame(moment(new Date(ev.date)))) {
-          return ev.event;
-        } else {
-          return "";
+  export default {
+    data: () => ({
+      today: '2019-01-08',
+      events: [
+        {
+          title: 'Vacation',
+          details: 'Going to the beach!',
+          date: '2018-12-30',
+          open: false
+        },
+        {
+          title: 'Vacation',
+          details: 'Going to the beach!',
+          date: '2018-12-31',
+          open: false
+        },
+        {
+          title: 'Vacation',
+          details: 'Going to the beach!',
+          date: '2019-01-01',
+          open: false
+        },
+        {
+          title: 'Meeting',
+          details: 'Spending time on how we do not have enough time',
+          date: '2019-01-07',
+          open: false
+        },
+        {
+          title: '30th Birthday',
+          details: 'Celebrate responsibly',
+          date: '2019-01-03',
+          open: false
+        },
+        {
+          title: 'New Year',
+          details: 'Eat chocolate until you pass out',
+          date: '2019-01-01',
+          open: false
+        },
+        {
+          title: 'Conference',
+          details: 'Mute myself the whole time and wonder why I am on this call',
+          date: '2019-01-21',
+          open: false
+        },
+        {
+          title: 'Hackathon',
+          details: 'Code like there is no tommorrow',
+          date: '2019-02-01',
+          open: false
         }
-      });
+      ]
+    }),
+    computed: {
+      // convert the list of events into a map of lists keyed by date
+      eventsMap () {
+        const map = {}
+        this.events.forEach(e => (map[e.date] = map[e.date] || []).push(e))
+        return map
+      }
     },
-    isSameMonth: function(day) {
-      return day.month() === this.currentMonth;
-    },
-    nextMonth() {
-      this.today.add(1, "month");
-      this.setMonth();
-      this.setYear();
-      this.setMounthWeek();
-    },
-    prevMonth() {
-      this.today.subtract(1, "month");
-      this.setMonth();
-      this.setYear();
-      this.setMounthWeek();
-    },
-    setMonth: function() {
-      this.currentMonth = this.today.month();
-    },
-    setYear: function() {
-      this.currentYear = this.today.year();
-    },
-    selectDay: function(day) {
-      this.activeDay = day.date;
-      this.daySelected = day;
+    methods: {
+      open (event) {
+        alert(event.title)
+      }
     }
-  },
-  mounted: function() {
-    // let recaptchaScript = document.createElement('script')
-    // recaptchaScript.async = true
-    // recaptchaScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js')
-    
-    // document.head.appendChild(recaptchaScript)
-    this.setMonth();
-    this.setYear();
-    this.setMounthWeek();
-    // console.log(this.calendar)
   }
-};
-
-// moment.locale('en')
-// const events = [{
-//   date: '02-10-2019',
-//   event: 'coucou les ptits loups'
-// },{
-//   date: '03-16-2019',
-//   event: 'Boom Party'
-// },
-//                ]
 </script>
